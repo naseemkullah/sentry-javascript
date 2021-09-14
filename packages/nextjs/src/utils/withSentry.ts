4d25243cb995c26471a43baa6f360b6ffd08d452
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-09-14 18:21:12 +0200

    Revert "Use an older handler"
    This reverts commit a58a6f1313fa50e5907e7d20c4332b4775125bf8.

--- packages/nextjs/src/utils/withSentry.ts
+++ packages/nextjs/src/utils/withSentry.ts
@@ -1,6 +1,8 @@
-import { captureException, flush, getCurrentHub, Handlers } from '@sentry/node';
+import { captureException, flush, getCurrentHub, Handlers, startTransaction } from '@sentry/node';
+import { extractTraceparentData, hasTracingEnabled } from '@sentry/tracing';
 import { Transaction } from '@sentry/types';
-import { addExceptionMechanism, logger } from '@sentry/utils';
+import { addExceptionMechanism, isString, logger, stripUrlQueryAndFragment } from '@sentry/utils';
+import * as domain from 'domain';
 import { NextApiHandler, NextApiResponse } from 'next';
 
 const { parseRequest } = Handlers;
@@ -10,33 +12,86 @@ type WrappedNextApiHandler = NextApiHandler;
 
 type AugmentedResponse = NextApiResponse & { __sentryTransaction?: Transaction };
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const withSentry = (handler: NextApiHandler): WrappedNextApiHandler => {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   return async (req, res) => {
     logger.log(`withSentry handler called...`);
+    // first order of business: monkeypatch `res.end()` so that it will wait for us to send events to sentry before it
+    // fires (if we don't do this, the lambda will close too early and events will be either delayed or lost)
+    // eslint-disable-next-line @typescript-eslint/unbound-method
     res.end = wrapEndMethod(res.end);
 
-    // con este codigo, funcionan los errores que pasan dentro del `withSentry` (el api error 3 y 4)
-    // los que pasan fuera, no se capturan (el 1 y el 2)
-    try {
-      return await handler(req, res); // Call Handler
-    } catch (e) {
-      logger.log('Error catched in the handler: ', e.message);
+    // use a domain in order to prevent scope bleed between requests
+    const local = domain.create();
+    local.add(req);
+    local.add(res);
+
+    // `local.bind` causes everything to run inside a domain, just like `local.run` does, but it also lets the callback
+    // return a value. In our case, all any of the codepaths return is a promise of `void`, but nextjs still counts on
+    // getting that before it will finish the response.
+    const boundHandler = local.bind(async () => {
       const currentScope = getCurrentHub().getScope();
+
       if (currentScope) {
-        currentScope.addEventProcessor(event => {
-          addExceptionMechanism(event, {
-            handled: false,
-          });
-          return parseRequest(event, req);
-        });
-        captureException(e);
+        currentScope.addEventProcessor(event => parseRequest(event, req));
+
+        if (hasTracingEnabled()) {
+          // If there is a trace header set, extract the data from it (parentSpanId, traceId, and sampling decision)
+          let traceparentData;
+          if (req.headers && isString(req.headers['sentry-trace'])) {
+            traceparentData = extractTraceparentData(req.headers['sentry-trace'] as string);
+            logger.log(`[Tracing] Continuing trace ${traceparentData?.traceId}.`);
+          }
+
+          const url = `${req.url}`;
+          // pull off query string, if any
+          let reqPath = stripUrlQueryAndFragment(url);
+          // Replace with placeholder
+          if (req.query) {
+            // TODO get this from next if possible, to avoid accidentally replacing non-dynamic parts of the path if
+            // they match dynamic parts
+            for (const [key, value] of Object.entries(req.query)) {
+              reqPath = reqPath.replace(`${value}`, `[${key}]`);
+            }
+          }
+          const reqMethod = `${(req.method || 'GET').toUpperCase()} `;
+
+          const transaction = startTransaction(
+            {
+              name: `${reqMethod}${reqPath}`,
+              op: 'http.server',
+              ...traceparentData,
+            },
+            // extra context passed to the `tracesSampler`
+            { request: req },
+          );
+          currentScope.setSpan(transaction);
+
+          // save a link to the transaction on the response, so that even if there's an error (landing us outside of
+          // the domain), we can still finish it (albeit possibly missing some scope data)
+          (res as AugmentedResponse).__sentryTransaction = transaction;
+        }
       }
-      throw e;
-    } finally {
-      logger.log('flushing...');
-      await flush(2000);
-    }
+
+      try {
+        return await handler(req, res); // Call original handler
+      } catch (e) {
+        logger.log('Error catched in the handler: ', e.message);
+        if (currentScope) {
+          currentScope.addEventProcessor(event => {
+            addExceptionMechanism(event, {
+              handled: false,
+            });
+            return event;
+          });
+          captureException(e);
+        }
+        throw e;
+      }
+    });
+
+    return await boundHandler();
   };
 };
 
@@ -44,6 +99,7 @@ type ResponseEndMethod = AugmentedResponse['end'];
 type WrappedResponseEndMethod = AugmentedResponse['end'];
 
 function wrapEndMethod(origEnd: ResponseEndMethod): WrappedResponseEndMethod {
+  console.log('wrapping end method');
   return async function newEnd(this: AugmentedResponse, ...args: unknown[]) {
     const transaction = this.__sentryTransaction;
 
